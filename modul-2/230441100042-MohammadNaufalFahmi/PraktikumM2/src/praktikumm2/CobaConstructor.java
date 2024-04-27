75b3ddef1c7778855e809067b3d2b23a50446df9
Your Name <you@example.com>
2024-04-27 23:14:21 +0700

    b

--- modul-2/230441100042-MohammadNaufalFahmi/PraktikumM2/src/praktikumm2/CobaConstructor.java
+++ modul-2/230441100042-MohammadNaufalFahmi/PraktikumM2/src/praktikumm2/CobaConstructor.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
@@ -34,3 +35,41 @@ public class CobaConstructor {
         Mahasiswa orang1 = new Mahasiswa("Bagas Dribble","23000765423","Dribble Basket");
     }
 }
+=======
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package praktikumm2;
+
+/**
+ *
+ * @author THINKPAD
+ */
+class Mahasiswa{
+        String Nama, NIM,Jurusan;
+        
+        Mahasiswa( String inputNama, String inputNIM, String inputJurusan){
+            Nama = inputNama;
+            NIM = inputNIM;
+            Jurusan = inputJurusan;
+            
+            System.out.println(Nama);
+            System.out.println(NIM);
+            System.out.println(Jurusan);
+            
+            System.out.println("Bisa berhasil kah Maniiiieeezzzz ?");
+        }
+    }
+public class CobaConstructor {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Mahasiswa orang1 = new Mahasiswa("Bagas Dribble","23000765423","Dribble Basket");
+    }
+}
+>>>>>>> e32242d26f94b837f14c0b58712fb0cbb81442e0

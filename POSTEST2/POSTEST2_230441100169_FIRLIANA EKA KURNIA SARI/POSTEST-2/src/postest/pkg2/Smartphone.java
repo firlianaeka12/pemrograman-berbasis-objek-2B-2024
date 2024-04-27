028bcb73adb09d9f0897dab1e7376492c242e49b
ASUS <ASUS@LAPTOP-4901B8OV>
2024-04-27 23:32:56 +0700

    Mengumpulkan POSTEST-2 ASPRAK KAK CINDY

---
+++ POSTEST2/POSTEST2_230441100169_FIRLIANA EKA KURNIA SARI/POSTEST-2/src/postest/pkg2/Smartphone.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package postest.pkg2;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Smartphone extends Handphone {
+    private int kapasitasMemori;
+    private int kameraPiksel;
+
+    public Smartphone(String merek, String model, int tahunProduksi, double harga, int kapasitasMemori, int kameraPiksel) {
+        super(merek, model, tahunProduksi, harga);
+        this.kapasitasMemori = kapasitasMemori;
+        this.kameraPiksel = kameraPiksel;
+    }
+
+    @Override
+    public void tampilkanSpesifikasi() {
+        System.out.println("Merek: " + merek);
+        System.out.println("Model: " + model);
+        System.out.println("Tahun Produksi: " + tahunProduksi);
+        System.out.println("Harga: Rp " + harga);
+        System.out.println("Kapasitas Memori: " + kapasitasMemori + " GB");
+        System.out.println("Kamera: " + kameraPiksel + " Piksel");
+    }
+}
+    
+

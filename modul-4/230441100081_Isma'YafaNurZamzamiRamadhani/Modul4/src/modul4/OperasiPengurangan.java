5a9118ce155442cbfb43a25658e8868862367cd3
ismaramadhani <ismaramadhani56@gmail.com>
2024-04-30 10:03:12 +0700

    230441100081_Isma'YafaNurZamzamiRamadhani_RachmadAzizFazarikha_Modul4

---
+++ modul-4/230441100081_Isma'YafaNurZamzamiRamadhani/Modul4/src/modul4/OperasiPengurangan.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modul4;
+
+/**
+ *
+ * @author HP VICTUS
+ */
+public class OperasiPengurangan extends OperasiBilanganAbs {
+    
+    protected void set_A(double a){
+        this.a = a;
+    }
+    protected void set_B(double b){
+        this.b = b;
+    }
+    protected void set_C(){
+        this.c = a - b;
+    }
+    protected double get_A(){
+        return a;
+    }
+    protected double get_B(){
+        return b;
+    }
+    protected double get_C(){
+        return c;
+    }
+    protected void tampil(){
+        System.out.println("Hasil Pengurangan");
+        System.out.println("Bilangan A = " + get_A());
+        System.out.println("Bilangan B = " + get_B());
+        System.out.println(get_A() + " - " + get_B() + " = " + get_C());
+        System.out.println("=================");
+    }
+}
---
+++ modul-4/230441100081_Isma'YafaNurZamzamiRamadhani/Modul4/src/modul4/OperasiPenjumlahan.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modul4;
+
+/**
+ *
+ * @author HP VICTUS
+ */
+public class OperasiPenjumlahan extends OperasiBilanganAbs {
+    
+    protected void set_A(double a){
+        this.a = a;
+    }
+    protected void set_B(double b){
+        this.b = b;
+    }
+    protected void set_C(){
+        this.c = a + b;
+    }
+    protected double get_A(){
+        return a;
+    }
+    protected double get_B(){
+        return b;
+    }
+    protected double get_C(){
+        return c;
+    }
+    protected void tampil(){
+        System.out.println("Hasil Penjumlahan");
+        System.out.println("Bilangan A = " + get_A());
+        System.out.println("Bilangan B = " + get_B());
+        System.out.println(get_A() + " + " + get_B() + " = " + get_C());
+        System.out.println("=================");
+    }
+    
+}

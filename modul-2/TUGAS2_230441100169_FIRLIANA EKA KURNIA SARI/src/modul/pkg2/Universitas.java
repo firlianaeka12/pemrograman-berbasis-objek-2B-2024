acbdbfabfbb32669c2c25e7b81a25ef93ee14ded
ASUS <ASUS@LAPTOP-4901B8OV>
2024-04-30 11:59:20 +0700

    230441100169_FIRLIANA EKA KURNIA SARI

---
+++ modul-2/TUGAS2_230441100169_FIRLIANA EKA KURNIA SARI/src/modul/pkg2/Universitas.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modul.pkg2;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Universitas {
+    
+ 
+    private static String namaUniversitas;
+
+    public static String getNamaUniversitas() {
+        return namaUniversitas;
+    }
+
+    public static void setNamaUniversitas(String nama) {
+        namaUniversitas = nama;
+    }
+}
+    
+

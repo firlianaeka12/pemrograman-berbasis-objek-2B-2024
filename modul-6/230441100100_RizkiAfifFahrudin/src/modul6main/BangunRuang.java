b033230cda1eab6f8e8c283daf65e1e981f495b8
Rizkiafif01 <rizkiafif00@gmail.com>
2024-05-13 17:59:54 +0700

    Pengumpulan_modul6_RizkiAfifFahrudin

---
+++ modul-6/230441100100_RizkiAfifFahrudin/src/modul6main/BangunRuang.java
@@ -0,0 +1,10 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modul6main;
+
+abstract class BangunRuang {
+    abstract double hitungVolume();
+    abstract void info();
+}
---
+++ modul-6/230441100100_RizkiAfifFahrudin/src/modul6main/Kerucut.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modul6main;
+
+/**
+ *
+ * @author asus
+ */
+public class Kerucut extends BangunRuang implements JariJari,Tinggi{
+    double JariJari;
+    double Tinggi;
+    
+    public Kerucut(double JariJari,double Tinggi){
+        this.JariJari=JariJari;
+        this.Tinggi=Tinggi;
+    }
+    
+    @Override
+    public double get_jarijari(){
+        return JariJari;
+    }
+    
+    @Override
+    public double get_tinggi(){
+        return Tinggi;
+    }
+    
+    @Override
+    double hitungVolume(){
+        return (1.0/3.0)*Math.PI*JariJari*JariJari*Tinggi;
+    }
+     @Override
+    void info(){
+        System.out.println("Volume Kerucut : "+ hitungVolume());
+    }
+    
+}

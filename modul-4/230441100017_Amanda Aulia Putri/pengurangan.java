69d235ee6ac7d7339d7ed8adf524c4cd878ff579
amandlyyptr <143859474+amandlyyptr@users.noreply.github.com>
2024-05-14 19:22:54 +0700

    230441100017_Amanda Aulia Putri_Modul4_AsprakKakPutri

---
+++ modul-4/230441100017_Amanda Aulia Putri/pengurangan.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modul.pkg4;
+
+/**
+ *
+ * @author AMANDA AULIA
+ */
+class OperasiPengurangan extends OperasiBilanganAbs {
+    @Override
+    protected void set_A(double a) {
+        this.a = a;
+    }
+
+    @Override
+    protected void set_B(double b) {
+        this.b = b;
+    }
+
+    @Override
+    protected void set_C() {
+        this.c = a - b;
+    }
+
+    @Override
+    protected double get_A() {
+        return a;
+    }
+
+    @Override
+    protected double get_B() {
+        return b;
+    }
+
+    @Override
+    protected double get_C() {
+        return c;
+    }
+
+    @Override
+    protected void tampil() {
+        System.out.println("Hasil Pengurangan: " + c);
+    }
+}
